1b7716bd7be19acdffd3b578affde5135005c4cf
zpadillo <zianpadidils@gmail.com>
2025-07-29 20:34:54 +0800

    Create firstcode.py
    first code which is a simple 2 digit calculator

---
+++ firstcode.py
@@ -0,0 +1,19 @@
+while True:
+    result=None
+    x=int(input("Enter your first number: "))
+    op=input("Enter the operator (+, -, *, /): ")
+    y=int(input("Enter the second number: "))
+    if (op=='+'):
+        result=x+y
+        print(f'The sum of {x} and {y} is {result}\n')
+    elif (op=='-'):
+        result=x-y
+        print(f'The difference of {x} and {y} is {result}\n')
+    elif (op=='*'):
+        result=x*y
+        print(f'The product of {x} and {y} is {result}\n')
+    elif (op=='/'):
+        result=x/y
+        print(f'The quotient of {x} and {y} is {result}\n')
+    else:
+        print('Invalid entry for operator. Please re-input\n')
